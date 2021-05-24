a2d494f4bda0b78e91886bd83a4b2c0c53f70c64
Vido <956388+Vido@users.noreply.github.com>
2021-05-24 20:18:52 -0300

    Endpoints

--- src/categories/tests.py
+++ src/categories/tests.py
@@ -1,3 +1,22 @@
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+class CategoriesTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('/categories.json')
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(len(response.context['customers']), 5)
+
+
+class BrandsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('/brands.json')
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(len(response.context['customers']), 5)
--- src/categories/views.py
+++ src/categories/views.py
@@ -1,3 +1,7 @@
+import json
+from pathlib import Path
+
+import pandas as pd
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,14 +11,20 @@ class CategoriesAPIView(APIView):
         STATIC JSON
     """
     def get(self, request):
-        return Response({'some': 'data'})
+        categories_path = (Path(__file__).parent / 'categories_dataset.csv').resolve()
+        csv_file = pd.read_csv(categories_path)
+        data = csv_file.to_json(orient="records")
+        return Response(json.loads(data))
 
 class BrandAPIView(APIView):
     """
-        STATIC JSON
+        STATIC JSON FOR BRANDS
     """
     def get(self, request):
-        return Response({'some': 'data'})
+        brands_path = (Path(__file__).parent / 'brands-neotrust.csv').resolve()
+        csv_file = pd.read_csv(brands_path)
+        data = csv_file.to_json(orient="records")
+        return Response(json.loads(data))
 
 class UniqueProductAPIView(APIView):
     """
